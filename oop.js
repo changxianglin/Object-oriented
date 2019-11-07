ee48e51785f338b787fdac082e7085b303307602
changxianglin <281532612@qq.com>
2019-11-08 02:20:37 +0800

    call apply

--- oop.js
+++ oop.js
@@ -1,14 +1,14 @@
 // Factory Function
-function createCircle(radius) {
-   return {
-    radius,
-    draw: function() {
-      console.log('draw')
-    }
-  }
-}
+// function createCircle(radius) {
+//    return {
+//     radius,
+//     draw: function() {
+//       console.log('draw')
+//     }
+//   }
+// }
 
-const circle = createCircle(1)
+// const circle = createCircle(1)
 
 
 //Constructor Function 
@@ -19,5 +19,16 @@ function Circle(radius){
   }
 }
 
-const anothe = new Circle(1)
-anothe.draw()
+Circle.call({}, 1)
+Circle.apply({}, 1)
+
+// const Circle1 = new Function('radius', `
+// this.radius = radius
+// this.draw = function() {
+//   console.log('draw')
+// }
+// `)
+
+// const circle = new Circle1(1)
+
+const another = new Circle(1)
