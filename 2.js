a31c1e7e2fb466c678ff8ae77d86751ee4671e02
changxianglin <281532612@qq.com>
2019-11-08 02:28:47 +0800

    basic type and preference type

---
+++ 2.js
@@ -0,0 +1,24 @@
+// let x = {value: 10}
+// let y = x
+
+// x.value = 20
+
+
+// let number = 10
+// function increase(number) {
+//   number++
+// }
+
+// increase(number)
+
+// console.log(number) // 10
+
+
+let obj = {value: 10}
+function increase(obj) {
+  obj.value++
+}
+
+increase(obj)
+
+console.log(obj) // { value: 11}
