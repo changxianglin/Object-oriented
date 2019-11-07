405f0d42f651f60c9988aa8adc4760a50cbdd2c0
changxianglin <281532612@qq.com>
2019-11-08 02:43:26 +0800

    hidden detail

--- 3.js
+++ 3.js
@@ -14,23 +14,45 @@
 
 // delete circle.location
 
+// function Circle(radius) {
+//   this.radius = radius
+//   this.draw = function() {
+//     console.log('draw')
+//   }
+// }
+
+// const circle = new Circle(10)
+
+// for (let key in circle) {
+//   if(typeof circle[key] !== 'function') {
+//     console.log(key, circle[key])
+//   }
+// }
+
+// const keys = Object.keys(circle)
+// console.log(keys)
+
+// if('radius' in circle) 
+//   console.log('Circle has a radius')
+
+
 function Circle(radius) {
   this.radius = radius
+
+  this.defaultLocation = { x: 0, y: 0}
+
+  this.computeOptimumLocation = function(factor) {
+    // ...
+  }
+
   this.draw = function() {
+    this.computeOptimumLocation(0.1)
+
     console.log('draw')
   }
 }
 
 const circle = new Circle(10)
 
-for (let key in circle) {
-  if(typeof circle[key] !== 'function') {
-    console.log(key, circle[key])
-  }
-}
-
-const keys = Object.keys(circle)
-console.log(keys)
-
-if('radius' in circle) 
-  console.log('Circle has a radius')
+circle.computeOptimumLocation()
+circle.draw()
