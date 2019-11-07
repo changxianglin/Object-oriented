7ad4d23fb20735e02bd93284f2c324468c624b43
changxianglin <281532612@qq.com>
2019-11-08 02:57:55 +0800

    object get and set

--- 3.js
+++ 3.js
@@ -59,24 +59,54 @@
 
 
 
+// function Circle(radius) {
+//   this.radius = radius
+
+//   let defaultLocation = { x: 0, y: 0}
+
+//   let computeOptimumLocation = function(factor) {
+//     // ...
+//   }
+
+//   this.draw = function() {
+//     computeOptimumLocation(0.1)
+//     // defaultLocation
+
+//     // this.radius
+
+//     console.log('draw')
+//   }
+// }
+
+// const circle = new Circle(10)
+// circle.draw()
+
+
 function Circle(radius) {
   this.radius = radius
 
   let defaultLocation = { x: 0, y: 0}
 
-  let computeOptimumLocation = function(factor) {
-    // ...
+  this.getDefaultLocation = function() {
+    return defaultLocation
   }
 
   this.draw = function() {
-    computeOptimumLocation(0.1)
-    // defaultLocation
-
-    // this.radius
-
     console.log('draw')
   }
+
+  Object.defineProperty(this, 'defaultLocation', {
+    get: function() {
+      return defaultLocation
+    },
+    set: function(value) {
+      if(!value.x || !value.y) 
+        throw new Error('Invalid location.')
+        defaultLocation = value
+    }
+  })
 }
 
 const circle = new Circle(10)
+circle.defaultLocation = 1
 circle.draw()
