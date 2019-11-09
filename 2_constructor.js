fb2c580565bbc0cca08d45e0d7299e3ce3ae8e51
changxianglin <281532612@qq.com>
2019-11-09 23:50:12 +0800

    constructor

---
+++ 2_constructor.js
@@ -0,0 +1,18 @@
+// Constructor
+
+function Book(title, author, year) {
+  this.title = title
+  this.author = author
+  this.year = year
+
+  this.getSummary = function() {
+    return `${this.title} was written by 
+    ${this.author} in ${this.year}`
+  }
+}
+
+// Instatiate an Object
+const book3 = new Book('Book One', 'Jone Doe', '2018')
+const book4 = new Book('Book Two', 'Jane Doe', '2019')
+
+console.log(book3.getSummary())
