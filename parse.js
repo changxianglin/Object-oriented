f00cccbfb8fcd19d306b8f62ecd6390b0a97d72e
changxianglin <281532612@qq.com>
2020-04-10 03:38:24 +0800

    finish parse

---
+++ parse.js
@@ -0,0 +1,172 @@
+var text = "<divviewsize':[790],'nextbtncls':'next','navcls':'ma_na','prevbtncls':'prev','activetriggercls':'hidden','circular':true,'easing':'easeoutstrong','autoplay':true,'duration':1,'contentcls':'macontent','effect':'scrollx','interval':4,'triggertype':'mouse'}\"><div id=\"ks-component2275\"><div><div><img></div></div></div><div id=\"ks-component2287\"><div><div><img></div></div></div><div><ul><li><a href=\"https://lukfook.tmall.com/shop/view_shop.htm?scene=taobao_shop\" target=\"_blank\"><img src=\"https://gdp.alicdn.com/imgextra/i1/512711107/O1CN013Tm1HC1K332Fm92q2_!!512711107.jpg\" alt=\"利益点_01.jpg\" usemap=\"#Map20200214\" border=\"0\"></a><map name=\"Map20200214\"><area coords=\"623,377,722,510\" href=\"https://detail.tmall.com/item.htm?spm=a212k0.12153887.0.0.4e60687dyfcNvR&amp;id=613701406770&amp;scene=taobao_shop\" target=\"_blank\"><area coords=\"527,728,529,730\" target=\"_blank\"><area coords=\"34,24,495,291\" href=\"https://detail.tmall.com/item.htm?spm=a212k0.12153887.0.0.4e60687dyfcNvR&amp;id=613456317296&amp;scene=taobao_shop\" target=\"_blank\"><area coords=\"457,391,576,503\" href=\"https://detail.tmall.com/item.htm?spm=a212k0.12153887.0.0.4e60687dyfcNvR&amp;id=613451953669&amp;scene=taobao_shop\" target=\"_blank\"></map></li><li><a href=\"https://lukfook.tmall.com/category-1232311066.htm?spm=a1z10.3-b-s.w4011-14987752190.16.680e568dAb9RT4&amp;tsearch=y&amp;scene=taobao_shop#TmshopSrchNav\" target=\"_blank\"><img src=\"https://gdp.alicdn.com/imgextra/i2/512711107/O1CN01hwUW3H1K331owpuF4_!!512711107.jpg\" alt=\"内页banner790x600.jpg\"></a></li></ul></div><div><div><ul><li></li><li>●</li></ul></div><div><ul><li>●&nbsp;</li><li>●</li></ul></div></div></divviewsize':[790],'nextbtncls':'next','navcls':'ma_na','prevbtncls':'prev','activetriggercls':'hidden','circular':true,'easing':'easeoutstrong','autoplay':true,'duration':1,'contentcls':'macontent','effect':'scrollx','interval':4,'triggertype':'mouse'}\">"
+
+// var text = "<p>00002480&nbsp;</p><div id=\"header\"><div><div id=\"logo\" onclick=\"javascript:location.href='/'\"><h1>OSCHINA.NET在线工具</h1></div></div></div><div id=\"mainContent\"><div>在线代码格式化<a href=\"https://tool.oschina.net/codeformat/html#advice\"><i></i>&nbsp;Feedback</a></div><div><div><ul><li><a href=\"https://tool.oschina.net/codeformat/html\">HTML格式化</a></li><li><a href=\"https://tool.oschina.net/codeformat/xml\">XML格式化</a></li><li><a href=\"https://tool.oschina.net/codeformat/css\">CSS格式化</a></li><li><a href=\"https://tool.oschina.net/codeformat/json\">JSON格式化</a></li><li><a href=\"https://tool.oschina.net/codeformat/js\">JavaScript格式化</a></li><li><a href=\"https://tool.oschina.net/codeformat/java\">Java格式化</a></li><li><a href=\"https://tool.oschina.net/codeformat/sql\">SQL格式化</a></li></ul><div></div></div><form id=\"codeformat_form\" action=\"https://tool.oschina.net/action/format/html\" method=\"POST\"><div>HTML格式化采用<code id=\"jsoup\" padding:=\"\" 2px=\"\" 4px;=\"\" font-family:=\"\" menlo,=\"\" monaco,=\"\" consolas,=\"\" &quot;courier=\"\" new&quot;,=\"\" monospace;=\"\" font-size:=\"\" 12px;=\"\" color:=\"\" rgb(221,=\"\" 17,=\"\" 68);=\"\" border-radius:=\"\" 3px;=\"\" background-color:=\"\" rgb(247,=\"\" 247,=\"\" 249);=\"\" border:=\"\" 1px=\"\" solid=\"\" rgb(225,=\"\" 225,=\"\" 232);\"=\"\"><a href=\"http://www.oschina.net/p/jsoup\" target=\"_blank\">Jsoup</a></code></div><div><div>待格式化HTML：</div><div><textarea name=\"html\" id=\"htmldata\"></textarea><div></div></div></div><div><input id=\"format\" type=\"submit\" value=\"格式化\">&nbsp;<input id=\"copy_format\" type=\"button\" value=\"复制格式化代码\"></div><div><div>格式化HTML：</div><div><textarea id=\"fhtml\" name=\"fhtml\"></textarea><div></div></div></div></form></div></div><div id=\"footer\"><p>在线工具 由&nbsp;<a href=\"http://www.oschina.net/\" target=\"_blank\">OSCHINA.NET</a>&nbsp;所有 |&nbsp;<a href=\"http://weibo.com/oschina2010\" target=\"_blank\">@新浪微博</a>&nbsp;|<a href=\"http://www.aliyun.com/\" target=\"_blank\">阿里云</a>提供服务器和带宽 |<a href=\"https://tool.oschina.net/codeformat/html#advice\">意见反馈</a>&nbsp;|&nbsp;<a href=\"http://www.beian.miit.gov.cn/\" target=\"_blank\">粤ICP备12009483号-6</a>&nbsp;|&nbsp;深圳市奥思网络科技有限公司版权所有</p></div>""
+
+// var content = text.split('>')
+
+// // console.log(content)
+
+const aSli = "<divviewsize':[790],'nextbtncls':'next','navcls':'ma_na','prevbtncls':'prev','activetriggercls':'hidden','circular':true,'easing':'easeoutstrong','autoplay':true,'duration':1,'contentcls':'macontent','effect':'scrollx','interval':4,'triggertype':'mouse'}\">"
+
+// const result = []
+
+// const syc = [':']
+// const split = ["'"]
+// const empty = [" "]
+// const illage = {
+//   sync: "'",
+//   split: ":",
+//   empty: " ",
+// }
+// content.map(item => {
+//   if(item.includes(illage.syc)) {
+//     result.push(item.split(illage.syc)[0])
+//   } else if(illage.split) {
+//     result.push(item.split(illage.split)[0])
+//   } else if(illage.empty) {
+//     result.push(item.split(illage.empty)[0])
+//   } else {
+//     result.push(item)
+//   }
+// })
+
+// console.log(result)
+
+
+//替换掉所有的 html标签，得到Html标签中的内容
+// var s=`<P><FONT face=宋体 color=#000000>    北京中航宇飞科技有限公司是一家致力于为中国国防工业服务的高新企业。<BR> <BR>    事业一部 主要以连接器、常异型热缩套管和电缆组件的外协加工为主。凭借泰兴这个得天独厚的连接器生产基地的优势，公司生产圆形J599、J598、XC、Y2、Y3、Y4、Y11X、Y17、Y50、YX、BNC、SMA等二十几个系列2800多个规格型号以及矩形J、D、PCB板接系列300多个规格型号。</P>`
+ 
+// var dd=s.replace(/<\/?.+?>/g,"");
+// console.log(dd, 111)
+// var dds=dd.replace(/ /g,"");//dds为得到后的内容
+// console.log(dds);
+
+// 匹配取值
+var getF = function(text) {
+  // var start = '>'
+  // var end = '<'
+  // var list = []
+  // var arr = []
+  // for(let i = 0; i < text.length; i++) {
+  //   if(text[i] === start && text[i + 1] !== end) {
+  //     list.push(i)
+  //   } else if(text[i] === end && text[i - 1] !== start && i > 0) {
+  //     arr.push(i)
+  //   }
+  // }
+
+  // console.log(list, arr)
+
+  var start = '<'
+  var end = '>'
+  var content = {
+    startTag: [],
+    closeTag: [],
+  }
+  
+  for(let i = 0; i < text.length; i++) {
+    if(text[i] === start) {
+      console.log(i, '开始')
+      content.startTag.push(i)
+    } else if(text[i] === end) {
+      console.log(i, '结束')
+      content.closeTag.push(i)
+    }
+  }
+
+  return content
+}
+
+// getF(text)
+
+// const txt = '<div id=\"ks-component2287\">'
+// console.log(txt.includes(' '))
+// console.log(txt.includes('"'))
+
+// console.log(s.slice(3, 30))
+
+// 分割间距
+const illage = {
+  syc: "'",
+  empty: " ",
+}
+
+let outerT = text
+const finish = []
+// 正真计算
+const cacle = function(obj) {
+  let total = null
+  const start = obj.start
+  const end = obj.end
+  const next = obj.next
+  const text = obj.text 
+  const template = text.slice(start, end)
+  console.log('模板', template)
+  const middle = text.split(template)
+  // console.log('中间', middle)
+  let result = null
+  const mark = 'img' // 图片可以
+  const link = '<a ' // 标签也可以
+  if(template.includes(mark) || template.includes(link)) {
+    result = template
+    console.log('img')
+  } else {
+    if(template.includes(illage.syc)) {
+      result = template.split(illage.syc)[0]
+      console.log('1')
+    } else if(template.includes(illage.empty)) {
+      result = template.split(illage.empty)[0]
+      console.log(2)
+    } else {
+      result = template
+    }
+  }
+  // const result = template.split("'")[0]
+  console.log('结果', result)
+  
+  console.log('合成', middle[0]+ result + middle[1])
+  total = middle[0]+ result + middle[1]
+
+  let current = text.slice(end, next)
+  console.log('获取文本', current, result + current)
+  finish.push(result + current)
+  // outerT = total
+  return total
+}
+
+const getSplit = getF(text)
+
+getSplit.startTag.map((item, index) => {
+  console.log(item, getSplit.closeTag[index])
+  let end = getSplit.closeTag[index]
+  let next = getSplit.startTag[index + 1]
+  var obj = {
+    text: text,
+    // text: outerT,
+    start: item,
+    end: end,
+    next: next
+  }
+  var result = cacle(obj) // 修正模板
+  // console.log('修正', result)
+})
+
+console.log('圆满完成', finish)
+
+console.log('最后结果', finish.join(''))
+
+// 必须用递归才能行
+// 1. 算出符合条件的位置
+// 2. 计算一次
+// 3. 累次计算
+// 4. 到条件不满足退出(两次获得的位置完全一致则退出)
+
+// 斐波那契
+
+// function fb1(n){
+//   if(n <= 2){
+//       return 1;    
+//   }else{
+//       return fb1(n-1) + fb1(n-2);
+//   }
+// }
